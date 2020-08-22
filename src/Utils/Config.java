cd191b6150c967214127588c2a7e0ae154dd5f2f
lagosmanuel <lagosmanual@gmail.com>
2020-08-22 17:22:33 -0300

    created packages and refactored the login form

--- src/Utils/Config.java
+++ src/Utils/Config.java
@@ -1,16 +1,15 @@
-package boletindigital;
+package Utils;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-public class Conexion {
+public class Config {
 
     public static String url = "jdbc:mysql://18.231.175.39:3306/boletindigital";
     public static String user = "root";
     public static String pass = "boletin123digital456";
     
-    public static BasicDataSource setearParametros(){
+    public static BasicDataSource setDBParams(){
 
-        
         BasicDataSource bs = new BasicDataSource();
         bs.setDriverClassName("com.mysql.jdbc.Driver");
         bs.setUsername(user);
